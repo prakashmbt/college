e2e8a76d92fe26879de1910e8c6131f9783f4054
Prakash Muthusamy <1542@MONTBLEU.LOC>
2023-11-16 10:39:49 +0530

    commit of dev

--- src/main/java/com/common/api/util/ErrorValidation.java
+++ src/main/java/com/common/api/util/ErrorValidation.java
@@ -4,27 +4,23 @@ package com.common.api.util;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import com.common.api.constant.APIFixedConstant;
-import com.common.api.resource.DepartmentService;
-import com.common.api.response.Department;
 import com.common.api.resource.StudentService;
 import com.common.api.response.Student;
 
 @Component
 public class ErrorValidation extends APIFixedConstant {
-	   
 	   @Autowired
-	   DepartmentService departmentService;
+	   private StudentService studentDao;
+	   
+	   private Student student;
 	   
 	   @Autowired
 	   NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-	   @Autowired
-	   private StudentService studentDao;
 		
 	   private final List<String> errors = new ArrayList<>();
 	   
@@ -39,8 +35,6 @@ public class ErrorValidation extends APIFixedConstant {
 	    public List<String> getErrors() {
 	        return errors;
 	    }
-    
-	  
 	    
     public ErrorValidation validateInput(Optional<Integer> id, Optional<Object> input) {
  	
@@ -51,21 +45,8 @@ public class ErrorValidation extends APIFixedConstant {
             Integer idValue = id.get();
             if (idValue < 0) {
                 errorResponse.addError(INVALID_ID_FORMAT);
-            } 
+            }
         }
-	    
-   
-        if(input.isPresent()){
-        	input.ifPresent(inputValue -> {           
-            if(inputValue instanceof Department) {
-    			Department department = (Department) inputValue;
-    			validateUtil.validateStringField(department.getDepartmentName(), "department_name", Optional.of(NAME_REGEX));
-    			errorResponse.addErrors(validateUtil.getErrors());
-    		}
-                       
-        });
-    
-        }	
 	    
          if(input.isPresent()){
         	input.ifPresent(inputValue -> {           
